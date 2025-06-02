70ff7cbb30b43055b821bb15788a97300dc0aa61
Babalola Paul <pbabalola27@gmail.com>
2025-06-02 07:56:58 +0100

    Add Vanta Globe effect implementation in vanta.globe.min.js and added the contact section

---
+++ scripts.js
@@ -0,0 +1,11 @@
+VANTA.GLOBE({
+    el: "#contact",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundAlpha: 0.00,
+})
