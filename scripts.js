e63bd739956f6c614a248a2fe28905dba8c4f093
Babalola Paul <pbabalola27@gmail.com>
2025-06-02 09:48:43 +0100

    Add ScrollReveal animations for enhanced visual effects

--- scripts.js
+++ scripts.js
@@ -9,3 +9,34 @@ VANTA.GLOBE({
     scaleMobile: 1.00,
     backgroundAlpha: 0.00,
 })
+
+
+// ScrollReveal({
+//     reset: true,
+//     distance: '60px',
+//     duration: 1000,
+//     delay: 200
+// });
+
+// ScrollReveal().reveal('header h1', { origin: 'top' });
+// ScrollReveal().reveal('.hero-content .intro h1, .hero-content .intro h2', { origin: 'left', interval: 100 });
+// ScrollReveal().reveal('.hero-icons a', { origin: 'bottom', interval: 100 });
+// ScrollReveal().reveal('.download-cv', { origin: 'bottom' });
+// ScrollReveal().reveal('.hero-image img', { origin: 'right' });
+
+// ScrollReveal().reveal('.introduction p', { origin: 'bottom', interval: 100 });
+
+// ScrollReveal().reveal('.competency-sec .key', { origin: 'top' });
+// ScrollReveal().reveal('.competency', { origin: 'bottom', interval: 100 });
+
+// ScrollReveal().reveal('.Technologies .tech', { origin: 'right', interval: 100 });
+
+// ScrollReveal().reveal('.projects .key', { origin: 'top' });
+// ScrollReveal().reveal('.project-card', { origin: 'bottom', interval: 200 });
+
+// ScrollReveal().reveal('.contact p, .contact h1', { origin: 'left' });
+// ScrollReveal().reveal('.form-container form', { origin: 'bottom' });
+
+// ScrollReveal().reveal('footer .footer-con h1', { origin: 'top' });
+// ScrollReveal().reveal('footer .footer-con div', { origin: 'bottom' });
+// ScrollReveal().reveal('footer .copy', { origin: 'bottom', delay: 100 });
